521141dd898e65d4de14f9468e42ef33d9ca141f
ravi <ravi.solanki146783@gmail.com>
2021-04-14 19:19:25 +0530

    by Ravi

---
+++ cnnapp/app/src/main/java/com/example/cnn_news_app/Activity/AlertsThatMatter.kt
@@ -0,0 +1,20 @@
+package com.example.cnn_news_app.Activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cnn_news_app.MainActivity
+import com.example.cnn_news_app.R
+import kotlinx.android.synthetic.main.activity_alerts_that_matter.*
+
+class AlertsThatMatter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alerts_that_matter)
+
+        btDone.setOnClickListener {
+            intent= Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
